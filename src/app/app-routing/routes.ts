570d63c6c48e534f0ebb1ed932becfac9b0492e0
p.zezyulkin <p.zezyulkin@itransition.com>
2021-08-19 12:37:41 +0300

    confusion/move dishdetail to separate page

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from "@angular/router";
 import { ContactComponent } from "../contact/contact.component";
+import { DishdetailComponent } from "../dishdetail/dishdetail.component";
 import { HomeComponent } from "../home/home.component";
 import { MenuComponent } from "../menu/menu.component";
 
@@ -7,5 +8,6 @@ export const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'menu', component: MenuComponent},
   {path: 'contacts', component: ContactComponent},
+  {path: 'dishdetail/:id', component: DishdetailComponent},
   {path: '', redirectTo: '/home', pathMatch:'prefix'}
 ]
--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,11 +1,25 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { DishService } from '../services/dish.service';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-dishdetail',
-  templateUrl: './dishdetail.component.html',
-  styleUrls: ['./dishdetail.component.scss']
+  templateUrl: './dishdetail.component.html'
 })
-export class DishdetailComponent {
-  @Input()
+export class DishdetailComponent implements OnInit {
   dish?: IDish;
+
+  constructor(private dishService: DishService,
+    private location: Location,
+    private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    const id = this.route.snapshot.params['id'];
+    this.dish = this.dishService.getDish(id);
+  }
+
+  goBack() {
+    this.location.back();
+  }
 }
