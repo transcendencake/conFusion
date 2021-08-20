4889210fc8af30d8cbc19b66aaddf0ec6f9de167
p.zezyulkin <p.zezyulkin@itransition.com>
2021-08-20 12:59:12 +0300

    confusion/extract path constant

--- src/app/shared/components/icons/implementations/home-icon.component.ts
+++ src/app/shared/components/icons/implementations/home-icon.component.ts
@@ -1,9 +1,10 @@
 import { Component } from "@angular/core";
 import { BaseIcon } from "../base-icon";
+import { TEMPLATE_PATH } from "./icons.const";
 
 @Component({
   selector: 'app-icon-home',
-  templateUrl: '../icons.component.html'
+  templateUrl: TEMPLATE_PATH
 })
 export class HomeIcon extends BaseIcon {
   constructor() {
--- src/app/shared/components/icons/implementations/list-icon.component.ts
+++ src/app/shared/components/icons/implementations/list-icon.component.ts
@@ -1,9 +1,10 @@
 import { Component } from "@angular/core";
 import { BaseIcon } from "../base-icon";
+import { TEMPLATE_PATH } from "./icons.const";
 
 @Component({
   selector: 'app-icon-list',
-  templateUrl: '../icons.component.html'
+  templateUrl: TEMPLATE_PATH
 })
 export class ListIcon extends BaseIcon {
   constructor() {
